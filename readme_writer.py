edc9fe443578b025ab5ced86a7011cacd8687e1a
AI-Pranto <pranto.roza.53@gmail.com>
2021-05-11 21:17:27 +0600

    update

--- readme_writer.py
+++ readme_writer.py
@@ -1,5 +1,5 @@
 new_readme = "ENDF-V8 Data Generator. \n"
-	"Processed ENDF-V8 endf in h5 file format using GitHub actions CI pipline.\n"
+"Processed ENDF-V8 endf in h5 file format using GitHub actions CI pipline."
 
 with open("README.md", "w") as text_file:
     text_file.write(new_readme)
